b2135ee6e4fca83529d87dbf73f7518cc06a331a
nd-02110114 <nd.12021218@gmail.com>
2020-03-22 22:55:42 +0900

    :sparkles: add codes

--- abc159/abc159_d/main.py
+++ abc159/abc159_d/main.py
@@ -1,28 +1,37 @@
 import collections
 from sys import stdin
 
+# # not AC
+# def counter(num_arr):
+#     uniq_dict = {}
+#     for val in num_arr:
+#         uniq_dict[val] = uniq_dict.get(val, 0) + 1
+#     return uniq_dict
 
-def counter(num_arr):
-    uniq_dict = {}
-    for val in num_arr:
-        uniq_dict[val] = uniq_dict.get(val, 0) + 1
-    return uniq_dict
 
+# def get_result(data):
+#     N = data[0][0]
+#     A = data[1]
+#     uniq_cnt = counter(A)
+#     ans = {}
+#     for key in uniq_cnt.keys():
+#         tmp = uniq_cnt.copy()
+#         tmp[key] -= 1
+#         ans[key] = int(sum([val * (val-1) / 2 for val in tmp.values()]))
+
+#     for i in range(N):
+#         print(ans[A[i]])
 
 def get_result(data):
     N = data[0][0]
     A = data[1]
-    uniq_cnt = counter(A)
-    item_dict = uniq_cnt.items()
+    uniq_cnt = collections.Counter(A)
+    all_selection = 0
+    for val in uniq_cnt.values():
+        all_selection += int(val * (val-1) / 2)
+
     for i in range(N):
-        ans = 0
-        for key, value in item_dict:
-            if key == A[i]:
-                value -= 1
-            if value == 1:
-                continue
-            ans += int(value * (value - 1) / 2)
-        print(ans)
+        print(all_selection - uniq_cnt[A[i]] + 1)
 
 
 if __name__ == '__main__':
