a48f70c521a72bcccc7ee25f746961f43893c43b
nd-02110114 <nd.12021218@gmail.com>
2020-04-11 14:48:24 +0900

    :memo: add readme

--- abc161/abc161_e/main.py
+++
@@ -1,3 +0,0 @@
-# Code for E - Yutori
-# Use input() to fetch data from STDIN
-print("Hello world")
--- abc161/abc161_f/main.py
+++
@@ -1,3 +0,0 @@
-# Code for F - Division or Substraction
-# Use input() to fetch data from STDIN
-print("Hello world")
