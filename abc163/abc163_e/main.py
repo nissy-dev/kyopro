a901d42cbfd164b2dff3894ff96f33be5f8e443c
nd-02110114 <nd.12021218@gmail.com>
2020-04-25 16:23:09 +0900

    :sparkles: add dp

--- abc163/abc163_e/main.py
+++ abc163/abc163_e/main.py
@@ -1,18 +1,22 @@
 from sys import stdin
 
 
-# def get_result(data):
-#     N, K = data[0]
-#     A = data[1]
-#     dp = [[-1]*(N+1) for _ in range(N+1)]
-#     B = [[A[i], i] for i in range(N)]
-#     B = sorted(B, reverse=True)
-#     ans = 0
-#     for r in range(K, N+2):
-#         min_val = e_cumsum[r]
-#         max_val = e_cumsum[N+1] - e_cumsum[N+1-r]
-#         ans += (max_val - min_val + 1) % mod
-#     return ans % mod
+# pypyでAC
+def get_result(data):
+    N = data[0][0]
+    A = data[1]
+    dp = [[0]*(N+1) for _ in range(N+1)]
+    B = [[A[i], i] for i in range(N)]
+    B = sorted(B, reverse=True)
+    for i in range(N):
+        for j in range(N-i):
+            dp[i+1][j] = max(dp[i+1][j], dp[i][j] + B[i+j][0] * (B[i+j][1] - i))
+            dp[i][j+1] = max(dp[i][j+1], dp[i][j] + B[i+j][0] * ((N-1-j) - B[i+j][1]))
+
+    res = 0
+    for i in range(N):
+        res = max(res, dp[i][N-i])
+    return res
 
 
 if __name__ == '__main__':
