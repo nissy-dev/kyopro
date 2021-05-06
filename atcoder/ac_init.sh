13ee2a8a1bd9124a7ec83017d4b10770e75452bc
nissy-dev <nd.12021218@gmail.com>
2021-05-06 14:27:51 +0900

    feat(atcoder): add h for dp

---
+++ atcoder/ac_init.sh
@@ -0,0 +1,1 @@
+atcoder-tools gen $1 --workspace . --lang python --template /Users/nissy-dev/code/github.com/nissy-dev/kyopro/atcoder/template.py
--- atcoder/dp/src/bin/h.rs
+++ atcoder/dp/src/bin/h.rs
@@ -1,3 +1,38 @@
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        // s: String,
+        // n: usize,
+        (h, w): (usize, usize),
+        // (a, b, c): (usize, usize, usize),
+        // data: [usize; n],
+        // data: [[char; w]; h]
+    }
+    let mut data = vec![vec![]; h];
+    for i in 0..h {
+        input! {
+            s: String
+        }
+        data[i] = s.chars().collect()
+    }
+
+    // dp[i][j] = (i,j)までの経路の数
+    let mut dp = vec![vec![0; w]; h];
+    dp[0][0] = 1;
+    let ans_mod = 10i32.pow(9) + 7;
+    for i in 0..h {
+        for j in 0..w {
+            if i + 1 < h && data[i + 1][j] == '.' {
+                dp[i + 1][j] = (dp[i + 1][j] + dp[i][j]) % ans_mod;
+            }
+
+            if j + 1 < w && data[i][j + 1] == '.' {
+                dp[i][j + 1] = (dp[i][j + 1] + dp[i][j]) % ans_mod;
+            }
+        }
+    }
+
+    println!("{}", dp[h - 1][w - 1]);
 }
