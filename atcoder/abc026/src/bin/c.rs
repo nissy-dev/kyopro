d9fde18480e8c34de8a91493dd2474d0d89cc3b2
nissy-dev <nd.12021218@gmail.com>
2021-05-09 01:27:02 +0900

    feat(atcoder): add abc

--- atcoder/abc026/src/bin/c.rs
+++ atcoder/abc026/src/bin/c.rs
@@ -1,3 +1,42 @@
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        // s: String,
+        n: usize,
+        // (n, m): (usize, usize),
+        // (a, b, c): (usize, usize, usize),
+        // data: [usize; n],
+        data: [[usize; 1]; n-1]
+    }
+
+    // 部下リスト
+    let mut subordinates: Vec<Vec<usize>> = vec![vec![]; n];
+    for i in 0..n {
+        for j in 0..(n - 1) {
+            if (i + 1) == data[j][0] {
+                subordinates[i].push(j + 1);
+            }
+        }
+    }
+
+    let mut salary = vec![1; n];
+    for i in (0..n).rev() {
+        if subordinates[i].len() == 1 {
+            salary[i] = 2 * salary[subordinates[i][0]] + 1;
+        }
+
+        if subordinates[i].len() > 1 {
+            let subordinates_salary = subordinates[i]
+                .iter()
+                .map(|&sub_num| salary[sub_num])
+                .collect::<Vec<usize>>();
+            salary[i] = subordinates_salary.iter().max().unwrap()
+                + subordinates_salary.iter().min().unwrap()
+                + 1;
+        }
+    }
+
+    println!("{}", salary[0]);
 }
--- atcoder/abc051/src/bin/c.rs
+++ atcoder/abc051/src/bin/c.rs
@@ -6,8 +6,30 @@ fn main() {
         // s: String,
         // n: usize,
         // (n, m): (usize, usize),
-        (sx, sy, tx, ty): (usize, usize, usize, usize),
+        (sx, sy, tx, ty): (isize, isize, isize, isize),
         // data: [usize; n],
         // data: [[usize; m]; n]
     }
+
+    // ただの考察問題
+    let mut ans = String::from("");
+    let dx = (tx - sx) as usize;
+    let dy = (ty - sy) as usize;
+    // path 1
+    ans += "U".repeat(dy).as_str();
+    ans += "R".repeat(dx).as_str();
+    // path 2
+    ans += "D".repeat(dy).as_str();
+    ans += "L".repeat(dx).as_str();
+    // path 3
+    ans += "L";
+    ans += "U".repeat(dy + 1).as_str();
+    ans += "R".repeat(dx + 1).as_str();
+    ans += "D";
+    // path 4
+    ans += "R";
+    ans += "D".repeat(dy + 1).as_str();
+    ans += "L".repeat(dx + 1).as_str();
+    ans += "U";
+    println!("{}", ans);
 }
