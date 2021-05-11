1b27924e838f0e92cde6a7cabb0e57b8caa80174
nissy-dev <nd.12021218@gmail.com>
2021-05-12 00:31:57 +0900

    feat(atcoder): fin binary search

--- atcoder/abc026/src/bin/d.rs
+++ atcoder/abc026/src/bin/d.rs
@@ -1,3 +1,25 @@
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        a: f64,
+        b: f64,
+        c: f64,
+    };
+
+    let mut l = 0_f64;
+    let mut r = 1_000_000_000_f64;
+    for _ in 0..100 {
+        let t = (r + l) / 2_f64;
+        // 関数に単調性があるので、100を境界値として二分探索する
+        let ft = a * t + b * (c * t * std::f64::consts::PI).sin();
+        if ft > 100_f64 {
+            r = t;
+        } else {
+            l = t;
+        }
+    }
+    let ans = r;
+    println!("{}", ans);
 }
