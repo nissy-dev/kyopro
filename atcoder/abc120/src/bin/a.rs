7a3552cc770d7026e4e1a33e41b55828ab26fb8d
nissy-dev <nd.12021218@gmail.com>
2021-06-23 23:55:34 +0900

    solve a

---
+++ atcoder/abc120/src/bin/a.rs
@@ -0,0 +1,21 @@
+use proconio::{fastout, input};
+// use proconio::marker::Chars;
+
+#[fastout]
+fn main() {
+    input! {
+        // s: String,
+        // n: usize,
+        // (n, m): (usize, usize),
+        (a, b, c): (usize, usize, usize),
+        // data: [usize; n],
+        // data: [[usize; m]; n]
+    }
+
+    let cnt = b / a;
+    if cnt > c {
+        println!("{}", c);
+    } else {
+        println!("{}", cnt);
+    }
+}
