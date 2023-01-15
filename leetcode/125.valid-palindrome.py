f46accc78276c6f9457ebe07107cf27b7cbac184
nissy-dev <nd.12021218@gmail.com>
2023-01-15 14:29:11 +0900

    add codes

--- leetcode/125.valid-palindrome.py
+++ leetcode/125.valid-palindrome.py
@@ -1,9 +1,25 @@
 import re
 
 
-# メモリ効率は悪い
-class Solution:
+# メモリ効率は悪い O(n)
+class _Solution:
     def isPalindrome(self, s: str) -> bool:
         s_re = re.sub(r"[^a-zA-Z0-9]+", "", s)
         str_list = list(s_re.lower())
         return str_list == str_list[::-1]
+
+
+# O(1) の解法
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        beg, end = 0, len(s) - 1
+        while beg <= end:
+            while not s[beg].isalnum() and beg < end:
+                beg += 1
+            while not s[end].isalnum() and beg < end:
+                end -= 1
+            if s[beg].lower() == s[end].lower():
+                beg, end = beg + 1, end - 1
+            else:
+                return False
+        return True
---
+++ leetcode/128.longest-consecutive-sequence.py
@@ -0,0 +1,41 @@
+# ソートする愚直な解法、O(nlogn)
+class _Solution:
+    def longestConsecutive(self, nums):
+        if not nums:
+            return 0
+
+        nums.sort()
+
+        longest_streak = 1
+        current_streak = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                if nums[i] == nums[i - 1] + 1:
+                    current_streak += 1
+                else:
+                    longest_streak = max(longest_streak, current_streak)
+                    current_streak = 1
+
+        return max(longest_streak, current_streak)
+
+
+# O(n) の解法
+class Solution:
+    def longestConsecutive(self, nums):
+        longest_streak = 0
+        num_set = set(nums)
+
+        for num in num_set:
+            # 連続しているものについては調査されているはずなので、スキップする
+            if num - 1 not in num_set:
+                current_num = num
+                current_streak = 1
+
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
+
+                longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
